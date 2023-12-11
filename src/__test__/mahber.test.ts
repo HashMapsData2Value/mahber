43607071e2c2db2e45fd16f8241d7a9881181cbd
HashMapsData2Value <83883690+HashMapsData2Value@users.noreply.github.com>
2023-12-11 22:33:33 +0100

    point representation works, not scalar

--- src/__test__/mahber.test.ts
+++ src/__test__/mahber.test.ts
@@ -25,14 +25,28 @@ describe('Mahber', () => {
     await appClient.create.createApplication({});
   });
 
-  test('add', async () => {
-    const sum = await appClient.add({});
-    console.log(sum);
+  test.skip('bls12_381g1_add', async () => {
+    const sum = await appClient.pointAdd({});
     expect(sum.return?.valueOf()).toStrictEqual([
       5, 114, 203, 234, 144, 77, 103, 70, 136, 8, 200, 235, 80, 169, 69, 12, 151, 33, 219, 48, 145, 40, 1, 37, 67, 144,
       45, 10, 195, 88, 166, 42, 226, 143, 117, 187, 143, 28, 124, 66, 195, 154, 140, 85, 41, 191, 15, 78, 22, 106, 157,
       140, 171, 198, 115, 163, 34, 253, 166, 115, 119, 157, 142, 56, 34, 186, 62, 203, 134, 112, 228, 97, 247, 59, 185,
       2, 29, 95, 215, 106, 76, 86, 217, 212, 205, 22, 189, 27, 186, 134, 136, 25, 121, 116, 157, 40,
     ]);
+  });
+
+  test('scalarMultBase', async () => {
+    const sum = await appClient.scalarMultBase({});
+    console.log(sum);
+    expect(sum.return?.valueOf()).toStrictEqual([
+      5, 137, 244, 216, 137, 136, 193, 189, 222, 252, 117, 169, 93, 186, 178, 170, 206, 245, 86, 120, 124, 230, 226,
+      125, 140, 167, 170, 25, 12, 157, 77, 114, 33, 155, 124, 133, 168, 95, 19, 126, 75, 243, 146, 32, 158, 2, 105, 155,
+      22, 210, 212, 191, 87, 123, 196, 223, 194, 178, 17, 21, 171, 101, 223, 235,
+    ]);
+  });
+
+  test.skip('pointCheck', async () => {
+    const sum = await appClient.pointSubgroup({});
+    expect(sum.return?.valueOf()).toStrictEqual(true);
   });
 });
--- src/contracts/mahber.algo.ts
+++ src/contracts/mahber.algo.ts
@@ -2,11 +2,11 @@ import { Contract } from '@algorandfoundation/tealscript';
 
 // eslint-disable-next-line no-unused-vars
 class Mahber extends Contract {
-  /**
+  /** Point add
    *
    * @returns The result of the operation
    */
-  add(): bytes {
+  pointAdd(): bytes {
     // @ts-ignore
     const result = ec_add(
       'BLS12_381g1',
@@ -19,4 +19,68 @@ class Mahber extends Contract {
     );
     return result;
   }
+
+  /** Scalar Mult
+   *
+   * @returns The result of the operation
+   */
+  scalarMult(): bytes {
+    // @ts-ignore
+    const result = ec_scalar_mul('BN254g1', hex(''), hex(''));
+    return result;
+  }
+
+  /** Scalar Mult Base
+   * Scalar multiplication of the base point
+   * @returns The result of the operation
+   */
+  scalarMultBase(): bytes {
+    // @ts-ignore
+    const result = ec_scalar_mul(
+      'BN254g1',
+      hex(
+        '00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000002'
+      ),
+      hex('01')
+    );
+    return result;
+  }
+
+  /** Point Check
+   *
+   * @returns The result of the operation
+   */
+  pointSubgroup(): boolean {
+    // @ts-ignore
+    const check1 = ec_subgroup_check(
+      'BN254g1',
+      hex(
+        '00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000002'
+      )
+    );
+    /**
+
+    const check2 = ec_subgroup_check(
+      'BN254g1',
+      hex(
+        '09d3a257b99f1ad804a9e2354ea71c72da7fa518f4ca7904c6951d924b4045b4174be12ae3fd899d55d3e487fa103f951a24ca0f670ecae802209b2518ccca6c'
+      )
+    );
+         * 
+     */
+
+    return check1; // && check2;
+  }
+
+  // /** Scalar Mult Base
+  //  *
+  //  * @returns The result of the operation
+  //  */
+  // hashToPoint(): bytes {
+  //   // @ts-ignore
+  //   const s = 'Hello World';
+  //   const h = sha256(s);
+  //   const result = ec_map_to('BN254g1', hex(h));
+  //   return result;
+  // }
 }
