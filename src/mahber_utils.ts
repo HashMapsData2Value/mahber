ed5b7dd8a2dfcc3269ec85cdf935249d3c84dfc7
HashMapsData2Value <83883690+HashMapsData2Value@users.noreply.github.com>
2023-12-11 12:25:54 +0100

    remove unnecessary utils

--- src/mahber_utils.ts
+++
@@ -1,9 +0,0 @@
-import { bls12_381 } from '@noble/curves/bls12-381';
-import * as utils from '@noble/curves/abstract/utils';
-import { AffinePoint } from '@noble/curves/abstract/curve';
-
-function concatXandY(p: AffinePoint<bigint>): Uint8Array {
-  return utils.concatBytes(utils.numberToBytesBE(p.x, 48), utils.numberToBytesBE(p.y, 48));
-}
-
-export const p = utils.bytesToHex(concatXandY(bls12_381.G1.ProjectivePoint.BASE.toAffine()));
