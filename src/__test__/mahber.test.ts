8f75b21fd3eb4ad24520a4a8bca6e3057aeeffe5
HashMapsData2Value <83883690+HashMapsData2Value@users.noreply.github.com>
2023-12-15 01:19:08 +0100

    feat: verifies ring of 2

--- src/__test__/mahber.test.ts
+++ src/__test__/mahber.test.ts
@@ -130,7 +130,7 @@ describe("Mahber", () => {
   //   ]);
   // });
 
-  test("challenge", async () => {
+  test.skip("challenge", async () => {
     const nonce = new Uint8Array([
       46, 156, 236, 248, 53, 50, 159, 170, 109, 169, 33, 158, 103, 180, 57, 80, 45, 125, 194, 252, 175, 92, 76, 230, 42,
       18, 31, 135, 164, 88, 110, 100,
@@ -150,7 +150,7 @@ describe("Mahber", () => {
       238, 181, 244, 155, 126, 196, 87, 125, 121, 0, 75, 219, 53,
     ]);
 
-    const msg = new Uint8Array([104, 101, 108, 108, 111]);
+    const msg = new Uint8Array([104, 101, 108, 108, 111]); // Hello
 
     const res = await appClient
       .compose()
@@ -167,9 +167,6 @@ describe("Mahber", () => {
       .dummyOpUp({ i: 10 })
       .dummyOpUp({ i: 11 })
       .dummyOpUp({ i: 12 })
-      .dummyOpUp({ i: 13 })
-      .dummyOpUp({ i: 14 })
-      .dummyOpUp({ i: 15 })
       .execute();
 
     const a = res.returns?.valueOf() as Array<bigint | Array<number>>;
@@ -178,5 +175,69 @@ describe("Mahber", () => {
       31, 23, 238, 26, 160, 14, 69, 231, 97, 21, 127, 13, 99, 116, 63, 10, 113, 187, 137, 219, 27, 244, 80, 61, 19, 14,
       234, 70, 151, 175, 55, 199,
     ]);
+  });
+
+  test("verify ring of 2", async () => {
+    const initializer = new Uint8Array([
+      2, 1, 168, 168, 197, 227, 109, 157, 227, 3, 13, 159, 58, 59, 188, 149, 247, 2, 237, 240, 102, 169, 201, 132, 53,
+      103, 207, 255, 209, 168, 137, 125,
+    ]);
+    const nonce1 = new Uint8Array([
+      0, 23, 143, 9, 79, 24, 233, 162, 162, 17, 38, 151, 172, 19, 67, 129, 94, 47, 155, 186, 40, 200, 20, 240, 65, 142,
+      242, 137, 167, 234, 228, 184,
+    ]);
+    const nonce2 = new Uint8Array([
+      6, 177, 96, 201, 104, 146, 205, 29, 185, 135, 79, 132, 98, 46, 200, 108, 59, 55, 244, 96, 252, 67, 22, 21, 97,
+      110, 133, 76, 124, 1, 178, 23,
+    ]);
+    const pk1 = new Uint8Array([
+      11, 48, 4, 239, 146, 88, 81, 181, 255, 165, 13, 48, 49, 236, 186, 215, 45, 128, 73, 145, 33, 162, 247, 59, 111,
+      204, 3, 250, 109, 60, 186, 154, 17, 236, 107, 153, 192, 187, 48, 92, 143, 116, 33, 82, 156, 186, 75, 116, 152, 83,
+      152, 91, 35, 51, 213, 130, 134, 113, 172, 108, 77, 224, 158, 148,
+    ]);
+    const pk2 = new Uint8Array([
+      9, 248, 102, 55, 220, 25, 60, 89, 46, 208, 84, 173, 48, 230, 88, 102, 32, 224, 91, 169, 142, 249, 223, 222, 61,
+      232, 97, 146, 42, 74, 99, 44, 33, 17, 116, 95, 48, 43, 184, 201, 193, 43, 213, 130, 27, 208, 47, 43, 35, 254, 22,
+      155, 150, 40, 132, 181, 229, 12, 225, 61, 204, 38, 70, 69,
+    ]);
+    const keyImage = new Uint8Array([
+      6, 251, 80, 216, 179, 51, 80, 107, 123, 176, 45, 6, 137, 30, 155, 94, 119, 47, 54, 163, 119, 54, 121, 145, 217,
+      199, 157, 38, 62, 98, 209, 83, 18, 233, 8, 27, 210, 8, 50, 237, 111, 248, 16, 77, 10, 111, 83, 140, 230, 177, 181,
+      186, 86, 237, 129, 180, 244, 65, 79, 179, 78, 0, 104, 198,
+    ]);
+    const msg = new Uint8Array([104, 101, 108, 108, 111]); // hello
+
+    const callChallenge = async (
+      nonce: Uint8Array,
+      cPrev: Uint8Array,
+      pk: Uint8Array,
+      keyImage: Uint8Array
+    ): Promise<Uint8Array> => {
+      const res = await appClient
+        .compose()
+        .challenge({ msg, nonce, cPrev, pk, keyImage })
+        .dummyOpUp({ i: 1 })
+        .dummyOpUp({ i: 2 })
+        .dummyOpUp({ i: 3 })
+        .dummyOpUp({ i: 4 })
+        .dummyOpUp({ i: 5 })
+        .dummyOpUp({ i: 6 })
+        .dummyOpUp({ i: 7 })
+        .dummyOpUp({ i: 8 })
+        .dummyOpUp({ i: 9 })
+        .dummyOpUp({ i: 10 })
+        .dummyOpUp({ i: 11 })
+        .dummyOpUp({ i: 12 })
+        .execute();
+      const a = res.returns?.valueOf() as Array<bigint | Array<number>>;
+      // @ts-ignore
+      return new Uint8Array(a[0]);
+    };
+
+    let c0 = initializer; // might seem unnecessary but here for pedagogical purposes
+    const c1 = await callChallenge(nonce1, c0, pk1, keyImage);
+    c0 = await callChallenge(nonce2, c1, pk2, keyImage);
+
+    expect(c0).toStrictEqual(initializer);
   });
 });
